b110bc9e67046293c79125711f487ab838cdfa71
Baleraks <98695234+Baleraks@users.noreply.github.com>
2022-03-05 18:07:47 +0300

    add house prefab

--- My project/Assets/Skripts/PlayerStats.cs
+++ My project/Assets/Skripts/PlayerStats.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    public GameObject Object;
     public static int money;
     public int startMoney = 0;
-
+   private void GetStartMoney()
+   {
+      
+   }
     private void Start()
     {
         money = startMoney;
