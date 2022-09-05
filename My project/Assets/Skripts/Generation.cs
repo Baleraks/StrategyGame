55285477775f6525f553f36be892c15d85cf24e3
taran2003 <taran2003@bk.ru>
2022-09-05 10:42:17 +0300

    Update map generation

--- My project/Assets/Skripts/Generation.cs
+++ My project/Assets/Skripts/Generation.cs
@@ -1,31 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Generation : MonoBehaviour
 {
-    public GameObject[] objects;
-    public GameObject castle;
+    [SerializeField] private GameObject[] objects;
+    [SerializeField] private GameObject castle;
+    private int edgeX = -2;
+    private int edgeZ = -2;
+    private int fieldeSize = 5;
+    private float fieldeMultiplier = 2.25f;
     private System.Random rand = new System.Random();
+
     void Start()
     {
         int indexToSpawn;
-        var indexX = -2.0f;
-        var indexZ = -2.0f;
-        Vector3 pointToSpawn = new Vector3(indexX*2.25f, 0.0f, indexZ*2.25f);
-        for(int i=0;i<5;i++)
+        var indexX = edgeX;
+        var indexZ = edgeZ;
+        Vector3 pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
+        for(int i=0;i< fieldeSize; i++)
         {
-            for(int j=0;j<5;j++)
+            for(int j=0;j< fieldeSize; j++)
             {
                 indexToSpawn = rand.Next(0, 4);
                 Instantiate(objects[indexToSpawn], pointToSpawn, new Quaternion(0.0f,0.0f,0.0f,0.0f));
                 indexX++;
-                pointToSpawn = new Vector3(indexX * 2.25f, 0.0f, indexZ * 2.25f);
+                pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
             }
-            indexX = -2.0f;
+            indexX = edgeX;
             indexZ++;
-            pointToSpawn = new Vector3(indexX * 2.25f, 0.0f, indexZ * 2.25f);
+            pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
         }
         Instantiate(castle, new Vector3(0,0.5f,0), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+        edgeX--; edgeZ--; fieldeSize += 2;
+    }
+
+    
+
+    public void Expansion()
+    {
+        int indexToSpawn;
+        var indexX = edgeX;
+        var indexZ = edgeZ;
+        Vector3 pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
+        for (int i = 0; i < fieldeSize; i++)
+        {
+            for (int j = 0; j < fieldeSize; j++)
+            {
+                if ((indexX == edgeX || indexX == Math.Abs(edgeX)) || (indexZ == edgeZ || indexZ == Math.Abs(edgeZ)))
+                {
+                    indexToSpawn = rand.Next(0, 4);
+                    Instantiate(objects[indexToSpawn], pointToSpawn, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+                    indexX++;
+                    pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
+                }
+                else
+                {
+                    indexX++;
+                    pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
+                }
+            }
+            indexX = edgeX;
+            indexZ++;
+            pointToSpawn = new Vector3(indexX * fieldeMultiplier, 0.0f, indexZ * fieldeMultiplier);
+        }
+        edgeX--; edgeZ--; fieldeSize += 2 ;
     }
 }
